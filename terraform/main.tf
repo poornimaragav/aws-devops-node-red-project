03d8764b08c230a1dda1c69916c8a8b123d16619
poornimaragav <102597416+poornimaragav@users.noreply.github.com>
2026-07-02 13:29:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,11 @@
 module "vpc" {
   source = "./modules/vpc"
-
-  project_name       = var.project_name
-  vpc_cidr           = var.vpc_cidr
-  public_subnet_cidr = var.public_subnet_cidr
-  private_subnet_cidr = var.private_subnet_cidr
 }
 
 module "security_group" {
   source = "./modules/security-group"
 
-  project_name = var.project_name
-  vpc_id       = module.vpc.vpc_id
-}
-
-module "iam" {
-  source = "./modules/iam"
-
-  project_name = var.project_name
+  vpc_id = module.vpc.vpc_id
 }
 
 module "ec2" {
@@ -26,8 +14,7 @@ module "ec2" {
   ami_id            = var.ami_id
   instance_type     = var.instance_type
   subnet_id         = module.vpc.public_subnet_id
-  security_group_id = module.security_group.security_group_id
-  instance_profile  = module.iam.instance_profile
+  security_group_id = module.security_group.sg_id
   key_name          = var.key_name
 }
 
@@ -35,10 +22,4 @@ module "s3" {
   source = "./modules/s3"
 
   bucket_name = var.bucket_name
-}
-
-module "cloudwatch" {
-  source = "./modules/cloudwatch"
-
-  instance_id = module.ec2.instance_id
 }
