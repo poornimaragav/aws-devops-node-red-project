58c7d867ace44406ddf38c7d4fac643daddf1b43
poornimaragav <102597416+poornimaragav@users.noreply.github.com>
2026-07-02 15:01:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,15 @@
-module "vpc" {
-  source = "./modules/vpc"
-}
-
-module "security_group" {
-  source = "./modules/security-group"
-
-  vpc_id = module.vpc.vpc_id
-}
-
 module "ec2" {
+
   source = "./modules/ec2"
 
   ami_id            = var.ami_id
+
   instance_type     = var.instance_type
-  subnet_id         = module.vpc.public_subnet_id
-  security_group_id = module.security_group.sg_id
+
+  subnet_id         = "subnet-123456789"
+
+  security_group_id = "sg-123456789"
+
   key_name          = var.key_name
-}
 
-module "s3" {
-  source = "./modules/s3"
-
-  bucket_name = var.bucket_name
 }
